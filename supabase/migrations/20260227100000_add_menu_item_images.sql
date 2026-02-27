e448d2b4ecd906ade3ffcbc08aee948dfd312b77
Andre Snell <andresnell29@gmail.com>
2026-02-27 09:40:51 +0200

    Add food images to all ~120 menu items
    - Unsplash food photography for every menu item
    - Images sized at 400px width for fast loading
    - Covers all 14 restaurants across all categories

---
+++ supabase/migrations/20260227100000_add_menu_item_images.sql
@@ -0,0 +1,152 @@
+-- Add food images to all menu items
+-- Using Unsplash food photography URLs
+
+-- ===== ROCKETS SANDTON =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400' WHERE name = 'Classic Smash Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1553979459-d2229ba7433b?w=400' WHERE name = 'BBQ Bacon Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?w=400' WHERE name = 'Chicken Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1544025162-d76694265947?w=400' WHERE name = 'Full Rack Ribs';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1529193591184-b1d58069ecdd?w=400' WHERE name = 'Half Rack Ribs';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1608039829572-9b1234ef1702?w=400' WHERE name = 'Buffalo Wings (12pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1630384060421-cb20aeb68713?w=400' WHERE name = 'Loaded Fries';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1639024471283-03518883512d?w=400' WHERE name = 'Onion Rings';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=400' WHERE name = 'Milkshake';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1621263764928-df1444c5e859?w=400' WHERE name = 'Craft Lemonade';
+
+-- ===== COL'CACCHIO SANDTON =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?w=400' WHERE name = 'Margherita';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1628840042765-356cda07504e?w=400' WHERE name = 'Pepperoni Classico';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=400' WHERE name = 'Pollo Fungi';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1590947132387-155cc02f3212?w=400' WHERE name = 'Prawn & Avo';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?w=400' WHERE name = 'Caesar Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1606313564200-e75d5e30476c?w=400' WHERE name = 'Chocolate Brownie';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?w=400' WHERE name = 'Tiramisu';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1560023907-5f339617ea55?w=400' WHERE name = 'Sparkling Water 500ml';
+
+-- ===== YAMATO SUSHI =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?w=400' WHERE name = 'Salmon Rose (8pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1617196034796-73dfa7b1fd56?w=400' WHERE name = 'Rainbow Roll (8pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1559410545-0bdcd187e0a6?w=400' WHERE name = 'Prawn Tempura Roll (8pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1534256958597-7fe685cbd745?w=400' WHERE name = 'Salmon Sashimi (10pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1580822184713-fc5400e7fe10?w=400' WHERE name = 'Mixed Sashimi Platter';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=400' WHERE name = 'Chicken Katsu Curry';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1564834724105-918b73d1b8e0?w=400' WHERE name = 'Edamame Beans';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1607301405390-d831c242f59b?w=400' WHERE name = 'Miso Soup';
+
+-- ===== DOPPIO ZERO BRYANSTON =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1513104890138-7c749659a591?w=400' WHERE name = 'Truffle Mushroom Pizza';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?w=400' WHERE name = 'Chicken Pesto Pasta';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?w=400' WHERE name = 'Prawn Linguine';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1608039829572-9b1234ef1702?w=400' WHERE name = 'Eggs Benedict';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1590301157890-4810ed352733?w=400' WHERE name = 'Açaí Bowl';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=400' WHERE name = 'Flat White';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1624353365286-3f8d62daad51?w=400' WHERE name = 'Chocolate Fondant';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?w=400' WHERE name = 'Fresh Orange Juice';
+
+-- ===== OCEAN BASKET BRYANSTON =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1579888944880-d98341245702?w=400' WHERE name = 'Fish & Chips';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1625943553852-781c6dd46faa?w=400' WHERE name = 'Grilled Prawns (6)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1599487488170-d11ec9c172f0?w=400' WHERE name = 'Calamari & Chips';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1615141982883-c7ad0e69fd62?w=400' WHERE name = 'Seafood Platter for 2';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585325701956-60dd9c8553bc?w=400' WHERE name = 'Prawn Rissoles (4)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=400' WHERE name = 'Greek Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1576618148400-f54bed99fcfd?w=400' WHERE name = 'Malva Pudding';
+
+-- ===== NANDO'S RANDBURG =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1598515214211-89d3c73ae83b?w=400' WHERE name = 'Quarter Chicken & 1 Side';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1594221708779-94832f4320d1?w=400' WHERE name = 'Half Chicken & 2 Sides';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1632778149955-e80f8ceca2e8?w=400' WHERE name = 'Full Chicken & 4 Sides';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1626700051175-6818013e1d4f?w=400' WHERE name = 'Chicken Wrap';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=400' WHERE name = 'Espetada (Full)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?w=400' WHERE name = 'Spicy Rice (Regular)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1625938145744-e380515399bf?w=400' WHERE name = 'Coleslaw';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1581006852262-e4307cf6283a?w=400' WHERE name = 'Bottomless Drink';
+
+-- ===== KUNG FU KITCHEN RANDBURG =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1525755662778-989d0524087e?w=400' WHERE name = 'Sweet & Sour Chicken';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585032226651-759b368d7246?w=400' WHERE name = 'Beef Chow Mein';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1603133872878-684f208fb84b?w=400' WHERE name = 'Chicken Fried Rice';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1496116218417-1a781b1c416c?w=400' WHERE name = 'Dim Sum Basket (6pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1548507200-e9e05eb25785?w=400' WHERE name = 'Spring Rolls (4pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1562967914-608f82629710?w=400' WHERE name = 'Honey Chilli Chicken';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1547592166-23ac45744acd?w=400' WHERE name = 'Wonton Soup';
+
+-- ===== SAKHUMZI RESTAURANT (SOWETO) =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=400' WHERE name = 'Mogodu (Tripe)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1547592180-85f173990554?w=400' WHERE name = 'Beef Stew & Pap';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1532550907401-a500c9a57435?w=400' WHERE name = 'Chicken Dust (Quarter)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1607116667573-37ec9e2c1a0d?w=400' WHERE name = 'Oxtail & Rice';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1543339308-d595c4974614?w=400' WHERE name = 'Chakalaka & Pap';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=400' WHERE name = 'Braai Platter for 2';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1535958636474-b021ee887b13?w=400' WHERE name = 'Umqombothi';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=400' WHERE name = 'Ginger Beer';
+
+-- ===== CHAF-POZI (SOWETO) =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1509722747041-616f39b57569?w=400' WHERE name = 'Kota (Quarter Loaf)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1509722747041-616f39b57569?w=400' WHERE name = 'Super Kota';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?w=400' WHERE name = 'Chaf Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1606755962773-d324e0a13086?w=400' WHERE name = 'Prego Chicken Roll';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1635790045517-17a40644dc72?w=400' WHERE name = 'Boerewors Roll';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1527477396000-e27163b481c2?w=400' WHERE name = 'Braai Chicken Wings (8pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585325701956-60dd9c8553bc?w=400' WHERE name = 'Fat Cakes (4pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=400' WHERE name = 'Mageu (500ml)';
+
+-- ===== THE GRILLHOUSE ROSEBANK =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1600891964092-4316c288032e?w=400' WHERE name = 'Fillet Steak 200g';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1558030006-450675393462?w=400' WHERE name = 'Sirloin 300g';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1588168333986-5078d3ae3976?w=400' WHERE name = 'Ribeye 400g';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1514516345957-556ca7d90a29?w=400' WHERE name = 'Lamb Cutlets';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1565557623262-b51c2513a641?w=400' WHERE name = 'Prawn Cocktail';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1580013759032-d5324755e4f0?w=400' WHERE name = 'Creamed Spinach';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1568569350062-ebfa3cb195df?w=400' WHERE name = 'Baked Potato';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1470124182917-cc6e71b22ecc?w=400' WHERE name = 'Crème Brûlée';
+
+-- ===== TASHAS ROSEBANK =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?w=400' WHERE name = 'Avo Toast';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=400' WHERE name = 'Salmon Poke Bowl';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=400' WHERE name = 'Grilled Chicken Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585325701956-60dd9c8553bc?w=400' WHERE name = 'Tashas Chicken Schnitzel';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1505576399279-565b52d4ac71?w=400' WHERE name = 'Beetroot & Quinoa Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1572442388796-11668a67e53d?w=400' WHERE name = 'Cappuccino';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?w=400' WHERE name = 'Iced Latte';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1616541823729-00fe0aacd32c?w=400' WHERE name = 'Red Velvet Cake';
+
+-- ===== LUCKY BEAN MELVILLE =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1533920379810-6bedac961555?w=400' WHERE name = 'Big Breakfast';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=400' WHERE name = 'Pancake Stack';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?w=400' WHERE name = 'Chicken Mayo Sandwich';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1550547660-d9450f859349?w=400' WHERE name = 'Beef Burger & Chips';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1540420773420-3366772f4999?w=400' WHERE name = 'Halloumi Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1611564494260-6f21b80af7ea?w=400' WHERE name = 'Cortado';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?w=400' WHERE name = 'Chai Latte';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585478259715-876acc5be8eb?w=400' WHERE name = 'Banana Bread';
+
+-- ===== ROCOMAMAS FOURWAYS =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400' WHERE name = 'OG Smash Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?w=400' WHERE name = 'Mexican Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?w=400' WHERE name = 'Nashville Hot Chicken Burger';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1630384060421-cb20aeb68713?w=400' WHERE name = 'Bacon & Cheese Loaded Fries';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1585109649139-366815a0d713?w=400' WHERE name = 'Chilli Cheese Fries';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1527477396000-e27163b481c2?w=400' WHERE name = 'Chicken Wings (6pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=400' WHERE name = 'Oreo Milkshake';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1581006852262-e4307cf6283a?w=400' WHERE name = 'Fanta Orange';
+
+-- ===== EL TACO LOCO FOURWAYS =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1565299585323-38d6b0865b47?w=400' WHERE name = 'Beef Tacos (3pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1626700051175-6818013e1d4f?w=400' WHERE name = 'Chicken Burrito';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1618040996337-56904b7850b9?w=400' WHERE name = 'Cheese Quesadilla';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1513456852971-30c0b8199d4d?w=400' WHERE name = 'Nachos Supreme';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?w=400' WHERE name = 'Fish Tacos (3pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1624353365286-3f8d62daad51?w=400' WHERE name = 'Churros (5pc)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1556855810-ac404aa91e85?w=400' WHERE name = 'Frozen Margarita';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1581006852262-e4307cf6283a?w=400' WHERE name = 'Mexican Coke';
+
+-- ===== CRAFT KITCHEN PARKHURST =====
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=400' WHERE name = 'Buddha Bowl';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?w=400' WHERE name = 'Grilled Linefish';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1514516345957-556ca7d90a29?w=400' WHERE name = 'Lamb Shoulder';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?w=400' WHERE name = 'Smoked Trout Salad';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1476124369491-e7addf5db371?w=400' WHERE name = 'Mushroom Risotto';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1452195100486-9cc805987862?w=400' WHERE name = 'Cheese Board';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1535958636474-b021ee887b13?w=400' WHERE name = 'Craft Beer (500ml)';
+UPDATE public.menu_items SET image_url = 'https://images.unsplash.com/photo-1563227812-0ea4c22e6cc8?w=400' WHERE name = 'Kombucha';
